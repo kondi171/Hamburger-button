91ca2c74e8c6701e84cc6a119f9cae672e29b0f4
kondi171 <kondi.171@wp.pl>
2018-06-29 00:16:31 +0200

    Add transition and design for button

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+var bodyEl = document.querySelector('body');
 var hamburgerBtn = document.querySelector('.hamburger-button');
 
 function expandList() {
@@ -10,7 +11,8 @@ hamburgerBtn.addEventListener('click', function () {
     var hamburgerTop = this.querySelector('#hamburger-top');
     var hamburgerMiddle = this.querySelector('#hamburger-middle');
     var hamburgerBottom = this.querySelector('#hamburger-bottom');
-    
+    bodyEl.classList.toggle('filter-b');
+    this.classList.toggle('p-fixed');
     this.classList.toggle('to-bottom');
     hamburgerTop.classList.toggle('top-transform');
     hamburgerMiddle.classList.toggle('mid-transform');
